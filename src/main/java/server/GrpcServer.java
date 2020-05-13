3aca57f0b1e591c8e85067fa3afc99dbc099c2a6
darkklord <ravi_yadav0502@yahoo.in>
2020-05-13 11:35:52 +0530

    Added vm flag in comments

--- src/main/java/server/GrpcServer.java
+++ src/main/java/server/GrpcServer.java
@@ -4,12 +4,16 @@ import java.io.IOException;
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import io.grpc.netty.NettyServerBuilder;
 
 public class GrpcServer {
     public static void main(String[] args) throws IOException, InterruptedException {
+    	//Pass this as VM arg before running	
+    	//-XX:MaxDirectMemorySize=4g
         Server server = ServerBuilder
           .forPort(9001)
-          .addService(new HelloServiceImpl()).build();
+          .addService(new HelloServiceImpl())
+          .build();
  
         server.start();
         System.out.println("Server started");
